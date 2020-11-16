ca78776a0ea64fa045b5481db70c870c74a94d29
tomasteel <luca@tomassi.ca>
2020-11-16 11:04:16 -0500

    Added Tests Folder

---
+++ Cross-Assembler for Virtual Machine/src/assembler/Tests/TestAdd.java
@@ -0,0 +1,13 @@
+package assembler.Tests;
+import java.io.*;
+
+public class TestAdd {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Test Add\n");
+        System.out.print("3\n"); //expected value
+        System.out.print(v1+v2); System.out.println();
+        //actual value
+        //setup pop(), setup stack[] 
+        
+    }
+}
